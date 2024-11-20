8060be9e2c02aadea7360be9d72eb47f78548422
shubhamsrnm <shubhamsrmn@gmail.com>
2024-11-20 16:49:21 +0530

    Updates

--- assets/Contacts-hBh_ZVSY.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CycAlGFc.js";const o=()=>t.jsx("div",{children:"Contacts"});export{o as default};
---
+++ assets/Projects-BYG6X-ZH.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BMYniD6f.js";const s=()=>r.jsx("div",{children:"Projects"});export{s as default};
