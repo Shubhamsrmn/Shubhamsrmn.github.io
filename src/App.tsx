961f6d96618dcc7dd9b1336e4c9bf3aa718fd0dc
shubhamsrnm <shubhamsrmn@gmail.com>
2024-11-20 16:09:27 +0530

    redux store for theme added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="font-urbanist tracking-wide text-primaryTextBlack flex justify-between">
+    <div className="font-urbanist tracking-wide text-primaryTextBlack flex justify-between min-h-screen">
       <Sidebar />
       <div className="flex-1 bg-primaryLightPink px-16 py-8 flex flex-col justify-between">
         <Outlet />
